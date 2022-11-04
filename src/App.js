5cdb61c7d2255e053ca83ffd3e4d411478c2b635
Jaisson Valbuena <jaissonvalbuena29@outlook.com>
2022-11-03 20:37:33 -0500

    Create new component and cahnges into screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomeScreen from "./screens/HomeScreen.js"
+import ProductScreen from "./screens/ProductScreen.js"
+import Navbar from "react-bootstrap/Navbar"
+import Container from "react-bootstrap/Container"
+import { LinkContainer } from "react-router-bootstrap"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mi primer proyecto con React js</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Tienda Virtual MinTic</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">@Derechos reservados Jaisson Valbuena</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
